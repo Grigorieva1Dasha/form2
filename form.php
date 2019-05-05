059b3dc494ab1c23f41d4986acacb53174deb7af
Grigorieva1Dasha <32798782+Grigorieva1Dasha@users.noreply.github.com>
2019-05-05 16:31:02 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thanks</title>
+</head>
+<body>
+	<h1>Спасибо, ваша заявка принята!</h1>
+	<p><a href="/ls/form">Отправить заявку еще раз.</p>
+</body>
+</html>
